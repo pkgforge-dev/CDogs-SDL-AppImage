cc39034367246c634382cb65f604d14363f4f72d
Link4Electronics <link4electronics@gmail.com>
2026-08-30 13:43:11 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -10,7 +10,7 @@ export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}
 export ICON=/usr/share/icons/hicolor/256x256/apps/io.github.cxong.cdogs-sdl.png
 export DESKTOP=/usr/share/applications/io.github.cxong.cdogs-sdl.desktop
 export STARTUPWMCLASS=cdogs-sdl
-export USE_HOST_DRIVERS_EXPERIMENTAL=1
+export DEPLOY_OPENGL=1
 
 # Deploy dependencies
 quick-sharun /usr/bin/cdogs-sdl
